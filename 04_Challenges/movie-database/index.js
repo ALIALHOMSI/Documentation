fe2d3b8a000d92656a49c665c8cefe9201bff1e4
ALI HOMSI <truphleg@gmail.com>
2023-01-10 12:05:03 +0200

    3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
-let port = 4000
+let port = 3000
 app.listen(port,function(req,res){
     console.log(`Server started at ${port}`)
 })
-app.get('/ali',(req,res) =>{
+app.get('/',(req,res) =>{
     res.send('ok')
+})
+app.get('/test',(req,res)=>{
+    res.send({status:200, message:"ok"})
+}
+
+)
+app.get('/time',(req,res)=>{
+    let date= new Date()
+    const obj={
+        status:200,
+        message:`${date.getHours()}:${date.getMinutes()}`
+
+    }
+    res.send(obj)
 })
