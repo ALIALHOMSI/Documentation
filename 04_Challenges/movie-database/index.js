ee09ca57e8db93565d4de26cf2c8eaea8eb80b80
ALI HOMSI <truphleg@gmail.com>
2023-01-10 12:45:16 +0200

    4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-let port = 3000
+let port = 5000
 app.listen(port,function(req,res){
     console.log(`Server started at ${port}`)
 })
@@ -21,3 +21,33 @@ app.get('/time',(req,res)=>{
     }
     res.send(obj)
 })
+app.get('/hello/:id?',(req,res)=>{
+   let id=req.params.id
+   let obj
+   obj={
+    status:200,
+    message:`Hello ,${id}`
+   }
+   res.send(obj)
+})
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+    
+        const response = {
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
+});
